4802de22ceb55c57e0efdb4d2b5ef41ee25d90f9
Waqar Ali Siyal <49365563+siraiwaqarali@users.noreply.github.com>
2020-10-21 23:55:06 +0500

    Add files via upload

---
+++ JS Projects/College Library/js/app.js
@@ -0,0 +1,112 @@
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+
+// Constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor
+function Display() {
+
+}
+
+// Add methods to display prototype
+Display.prototype.add = function (book) {
+    // console.log('Book Added');
+    let tableBody = document.getElementById('tableBody');
+    let tableRow = `
+        <tr>
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.type}</td>
+        </tr>
+    `;
+    tableBody.innerHTML += tableRow;
+}
+
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    let boldText;
+    if (type === 'success') {
+        boldText = 'Success:';
+    }
+    else {
+        boldText = 'Error!';
+    }
+    message.innerHTML = `
+        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+            <strong>${boldText} </strong> ${displayMessage}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    `;
+
+    setTimeout(() => {
+        message.innerHTML = '';
+    }, 5000);
+}
+
+
+// Add submit event listener to form
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    // console.log('You have submitted library form');
+    let name = document.getElementById('inputBookName').value;
+    let author = document.getElementById('inputAuthor').value;
+    let type;
+
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    // console.log(book);
+    let display = new Display();
+
+    if (display.validate(book)) {
+        // Add Book
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added');
+    }
+    else {
+        // show error
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
+}
+
